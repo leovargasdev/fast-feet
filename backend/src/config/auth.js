ef74f4359b7f2b7c2e0bbbc07036470b9893c97b
Leonardo Vargas <leu1607@hotmail.com>
2020-02-06 22:15:37 -0300

    Criando o controller para sessão, gerando o token de autenticação e adicionando pacotes ao projeto

---
+++ backend/src/config/auth.js
@@ -0,0 +1,5 @@
+export default {
+  // chave MD5: fastfeetbootcamp
+  secret: '93007edcc5c2fbfdf57a7396c9ca6c5d',
+  expiresIn: '7d',
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ main: 'Hello FastFeet' }));
+routes.post('/', SessionController.store);
 
 export default routes;
