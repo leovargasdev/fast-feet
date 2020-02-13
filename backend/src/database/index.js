aab7240db0325f5769b8a11a4ad304bdd53f4c17
Leonardo Vargas <leu1607@hotmail.com>
2020-02-13 09:59:13 -0300

    PQP ESSA BOSTA DE DELIVERYMAN NÃO FUNCIONAAAAAA

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -3,11 +3,10 @@ import Sequelize from 'sequelize';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Recipient from '../app/models/Recipient';
-import Deliveryman from '../app/models/Deliveryman';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient, File, Deliveryman];
+const models = [User, Recipient, File];
 
 class Database {
   constructor() {
