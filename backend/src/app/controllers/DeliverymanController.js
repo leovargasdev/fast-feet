7e7b99e7dda3de9bbf4ad3c9a01cb0079330cb70
Leonardo Vargas <leu1607@hotmail.com>
2020-02-13 09:04:01 -0300

    Arrumando o bug relacionado a tabela Entragadores

--- backend/src/app/controllers/DeliverymanController.js
+++ backend/src/app/controllers/DeliverymanController.js
@@ -1,26 +1,26 @@
-// import * as Yup from 'yup';
+import * as Yup from 'yup';
 
-// import Deliveryman from '../models/Deliveryman';
-// import File from '../models/File';
+import Deliveryman from '../models/Deliveryman';
+import File from '../models/File';
 
 class DeliverymanController {
   async store(req, res) {
-    // const schema = Yup.object().shape({
-    //   name: Yup.string().required(),
-    //   avatar_id: Yup.number().required(),
-    //   email: Yup.string()
-    //     .email()
-    //     .required(),
-    // });
-    // // if (!(await schema.isValid(req.body)))
-    // //   return res.status(400).json({ error: 'Validation fails.' });
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      avatar_id: Yup.number().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+    });
+    if (!(await schema.isValid(req.body)))
+      return res.status(400).json({ error: 'Validation fails.' });
 
-    // // const isAvatar = await File.findByPk(req.body.avatar_id);
-    // // if (!isAvatar) return res.status(400).json({ error: true });
+    const isAvatar = await File.findByPk(req.body.avatar_id);
+    if (!isAvatar) return res.status(400).json({ error: true });
 
-    // const { id, name, email } = await Deliveryman.create(req.body);
+    const { id, name, email } = await Deliveryman.create(req.body);
 
-    return res.json({ ok: true });
+    return res.json({ id, name, email });
   }
 }
 
