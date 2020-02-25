814edc27275d491eed3f87dfc7e4cfc1bf0c07d6
Leonardo Vargas <leu1607@hotmail.com>
2020-02-25 16:46:33 -0300

    Configurações iniciais do frontend. Adicionando pacotes, configurando o eslint e o babel

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Profile from '~/pages/Profile';
+
+function App() {
+  return (
+    <div>
+      <Profile />
+      <h1>Olar</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Profile() {
+  return <h1>Profile</h1>;
+}
