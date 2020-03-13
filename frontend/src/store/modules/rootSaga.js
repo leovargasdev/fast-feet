18d161edc2961353a05c2daa76d55ff8495fcec9
LeeonardoVargas <leu1607@hotmail.com>
2020-03-13 18:38:01 -0300

    Removendo algumas funções do redux

--- frontend/src/store/modules/rootSaga.js
+++ frontend/src/store/modules/rootSaga.js
@@ -2,10 +2,7 @@ import { all } from 'redux-saga/effects';
 
 import auth from './auth/sagas';
 import user from './user/sagas';
-import delivery from './delivery/sagas';
-import deliveryman from './deliveryman/sagas';
-import recipient from './recipient/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth, user, delivery, deliveryman, recipient]);
+  return yield all([auth, user]);
 }
