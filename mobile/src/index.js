0ece09f7f44f1687e7dfa497bc73a48eecb592be
Leonardo Vargas <leu1607@hotmail.com>
2020-03-20 18:50:46 -0300

    Adicionando versão mobile da aplicação

---
+++ mobile/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <View>
+      <Text>OLAR</Text>
+      <Text>OLAR</Text>
+      <Text>OLAR</Text>
+      <Text>OLAR</Text>
+      <Text>OLAR</Text>
+      <Text>OLAR</Text>
+      <Text>OLAR</Text>
+      <Text>OLAR</Text>
+    </View>
+  );
+}
