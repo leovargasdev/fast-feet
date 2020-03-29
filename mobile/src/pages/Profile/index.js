8cc299b1ea2ba506db5a1b23ab66ea0f6d66cf7d
Leonardo Vargas <leu1607@hotmail.com>
2020-03-29 18:25:02 -0300

    Finalizando o layout do perfil do entregador

--- mobile/src/pages/Profile/index.js
+++ mobile/src/pages/Profile/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {useDispatch} from 'react-redux';
 
-import {Container} from './styles';
+import {signOut} from '~/store/modules/auth/actions';
+import {Container, Content, Avatar, Label, Info, BtnLogout} from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
+
+  function handleLogout() {
+    dispatch(signOut());
+  }
+
   return (
     <Container>
-      <Text>Profile</Text>
+      <Content>
+        <Avatar
+          source={{
+            uri: `https://api.adorable.io/avatars/50/fastfeet.png`,
+          }}
+        />
+
+        <Label>Nome completo</Label>
+        <Info>Gaspar Antunes</Info>
+
+        <Label>Email</Label>
+        <Info>example@rocketseat.com.br</Info>
+
+        <Label>Data de cadastro</Label>
+        <Info>10/01/2020</Info>
+
+        <BtnLogout onPress={handleLogout}>Sair do Sistema</BtnLogout>
+      </Content>
     </Container>
   );
 }
--- mobile/src/pages/Profile/styles.js
+++ mobile/src/pages/Profile/styles.js
@@ -1,6 +1,45 @@
 import styled from 'styled-components/native';
+import {Platform} from 'react-native';
+
+import Button from '~/components/Button';
 
 export const Container = styled.View`
   flex: 1;
   background: #fff;
+`;
+
+export const Content = styled.KeyboardAvoidingView.attrs({
+  enabled: Platform.OS === 'ios',
+  behavior: 'padding',
+})`
+  flex: 1;
+
+  justify-content: center;
+  padding: 0 30px;
+`;
+
+export const Avatar = styled.Image`
+  align-self: center;
+  border-radius: 70px;
+  width: 140px;
+  height: 140px;
+  padding-bottom: 20px;
+`;
+
+export const Label = styled.Text`
+  margin-top: 20px;
+  color: #666666;
+  font-size: 12px;
+`;
+
+export const Info = styled.Text`
+  font-size: 22px;
+  font-weight: bold;
+  color: #444444;
+`;
+
+export const BtnLogout = styled(Button)`
+  width: 100%;
+  margin-top: 30px;
+  background: #e74040;
 `;
