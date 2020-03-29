2bbf2f3d471e16a4a3803fbe22ae0f91e04cdeb2
Leonardo Vargas <leu1607@hotmail.com>
2020-03-29 17:08:24 -0300

    Estruturando o Header do dashboardo com as informações do entregador, criando função de logout

--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -1,12 +1,45 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {useDispatch} from 'react-redux';
+import {TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {Container} from './styles';
+import {signOut} from '~/store/modules/auth/actions';
+
+import {
+  Container,
+  Header,
+  Profile,
+  Avatar,
+  Info,
+  Welcome,
+  Name,
+} from './styles';
 
 export default function Dashboard() {
+  const dispatch = useDispatch();
+
+  function handleLogout() {
+    dispatch(signOut());
+  }
+
   return (
     <Container>
-      <Text>Dashboard</Text>
+      <Header>
+        <Profile>
+          <Avatar
+            source={{
+              uri: `https://api.adorable.io/avatars/50/fastfeet.png`,
+            }}
+          />
+          <Info>
+            <Welcome>Bem vindo de volta,</Welcome>
+            <Name>Gaspar Antunes</Name>
+          </Info>
+        </Profile>
+        <TouchableOpacity onPress={handleLogout}>
+          <Icon name="exit-to-app" size={20} color="#f64c75" />
+        </TouchableOpacity>
+      </Header>
     </Container>
   );
 }
--- mobile/src/pages/Dashboard/styles.js
+++ mobile/src/pages/Dashboard/styles.js
@@ -1,6 +1,45 @@
 import styled from 'styled-components/native';
 
+import Button from '~/components/Button';
+
 export const Container = styled.View`
   flex: 1;
   background: #fff;
+`;
+export const Header = styled.View`
+  margin: 10px 20px 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Profile = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Avatar = styled.Image`
+  border-radius: 34px;
+  width: 68px;
+  height: 68px;
+`;
+
+export const Info = styled.View`
+  margin-left: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Welcome = styled.Text`
+  color: #666666;
+  font-size: 12px;
+  margin-bottom: -3px;
+`;
+
+export const Name = styled.Text`
+  font-weight: bold;
+  font-size: 22px;
+  color: #444;
 `;
