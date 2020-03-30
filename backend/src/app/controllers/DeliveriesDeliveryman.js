7cb1f1ffc71480ed589860ec6a57947d0d9b27e8
Leonardo Vargas <leu1607@hotmail.com>
2020-03-30 17:37:07 -0300

    Corrigindo médoto da api

--- backend/src/app/controllers/DeliveriesDeliveryman.js
+++ backend/src/app/controllers/DeliveriesDeliveryman.js
@@ -1,28 +1,19 @@
-
-import {format} from 'date-fns';
-
 import Delivery from '../models/Delivery';
 import Recipient from '../models/Recipient';
 
-class DeliveriesDeliveryman{
-  async index(req, res){
+class DeliveriesDeliveryman {
+  async index(req, res) {
     const deliveries = await Delivery.findAll({
-      where: {deliveryman_id: req.userId},
-      attributes: ['id','status', 'createdAt'],
+      where: { deliveryman_id: req.userId, canceled_at: null },
+      attributes: ['id', 'status', 'createdAt'],
       include: {
         model: Recipient,
         as: 'recipient',
-        attributes: ['city']
-      }
+        attributes: ['city'],
+      },
     });
-    const data = deliveries.map((delivery) => {
-      const {createdAt, status, id, recipient: {city}} = delivery;
-      return {
-        id, city, status,
-        date: format(createdAt, 'dd/MM/yyyy'),
-      }
-    });
-    return res.json(data);
+
+    return res.json(deliveries);
   }
 }
 
