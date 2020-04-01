ad274171012883f67377de293fd2be1cb09d982f
Leonardo Vargas <leu1607@hotmail.com>
2020-04-01 14:37:09 -0300

    Finalizando função para relatar problemas no encomenda

--- mobile/src/pages/Delivery/NewProblem/index.js
+++ mobile/src/pages/Delivery/NewProblem/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import {Text} from 'react-native';
+import PropTypes from 'prop-types';
+import React, {useState} from 'react';
 
 import BoxNavigate from '~/pages/Delivery/BoxNavigate';
+import {Container, Problem, BtnSubmit} from './styles';
 
-export default function NewProblem() {
+import api from '~/services/api';
+
+export default function NewProblem({route, navigation}) {
+  const {id} = route.params;
+
+  const [description, setDescription] = useState('');
+
+  async function handleSubmit() {
+    await api.post(`/delivery/${id}/problems`, {description});
+    navigation.goBack();
+  }
+
   return (
     <BoxNavigate>
-      <Text>NewProblem</Text>
+      <Container>
+        <Problem
+          style={{textAlignVertical: 'top'}}
+          multiline
+          numberOfLines={10}
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Inclua aqui o problema que ocorreu na entrega."
+          value={description}
+          onChangeText={setDescription}
+        />
+        <BtnSubmit onPress={handleSubmit}>Enviar</BtnSubmit>
+      </Container>
     </BoxNavigate>
   );
 }
+
+NewProblem.propTypes = {
+  route: PropTypes.element.isRequired,
+  navigation: PropTypes.element.isRequired,
+};
