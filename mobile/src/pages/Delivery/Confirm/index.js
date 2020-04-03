10afeffd3648a7a6ff5950a1a1a9706c593e7520
Leonardo Vargas <leu1607@hotmail.com>
2020-04-02 21:44:40 -0300

    Configurando o react-native-camera

--- mobile/src/pages/Delivery/Confirm/index.js
+++ mobile/src/pages/Delivery/Confirm/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import {Text} from 'react-native';
-
+import {View, TouchableOpacity, Text} from 'react-native';
+import {RNCamera} from 'react-native-camera';
 import BoxNavigate from '~/pages/Delivery/BoxNavigate';
 
 export default function Confirm() {
+  async function takePicture() {
+    if (this.camera) {
+      const options = {quality: 0.5, base64: true};
+      const data = await this.camera.takePictureAsync(options);
+      alert(data.uri);
+    }
+  }
+
   return (
     <BoxNavigate>
-      <Text>Confirm</Text>
+      <RNCamera
+        ref={camera => {
+          this.camera = camera;
+        }}
+        type={RNCamera.Constants.Type.back}
+        autoFocus={RNCamera.Constants.AutoFocus.on}
+        flashMode={RNCamera.Constants.FlashMode.off}
+        permissionDialogTitle="Permission to use camera"
+        permissionDialogMessage="We need your permission to use your camera phone"
+      />
+      <View>
+        <TouchableOpacity onPress={takePicture}>
+          <Text> SNAP </Text>
+        </TouchableOpacity>
+      </View>
     </BoxNavigate>
   );
 }
