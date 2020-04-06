906e51feb5ea3ea83893de4afe3f915b3a6d47a5
Leonardo Vargas <leu1607@hotmail.com>
2020-04-06 20:44:31 -0300

    Ajustes no estilo do status da entrega

--- frontend/src/pages/Delivery/List/styles.js
+++ frontend/src/pages/Delivery/List/styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
-import { lighten } from 'polished';
+import { lighten, darken } from 'polished';
 
 const colorsStatus = {
-  PENDENTE: '#C1BC35',
+  PENDENTE: '#ccc610',
   RETIRADA: '#4D85EE',
   ENTREGUE: '#2CA42B',
   CANCELADA: '#DE3B3B',
@@ -34,18 +34,20 @@ export const Deliveryman = styled.div`
 export const Status = styled.div`
   width: auto;
   margin: 0;
+
   span {
+    font-size: 15px;
     padding: 7px 10px;
-    background: ${props => lighten(0.3, colorsStatus[props.color])};
-    border-radius: 12px;
-    color: ${props => colorsStatus[props.color]};
+    background: ${props => darken(0.1, colorsStatus[props.color])};
+    border-radius: 8px;
+    color: rgba(255, 255, 255, 0.9);
     font-weight: bold;
 
     &::before {
       display: inline-block;
       width: 10px;
       height: 10px;
-      background: ${props => colorsStatus[props.color]};
+      background: ${props => lighten(0.3, colorsStatus[props.color])};
       content: '';
       border-radius: 50%;
       margin-right: 5px;
